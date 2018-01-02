0a9261157b8c0ab1bde18b1f29b330d677912a3e
Prodicode <tudor@prodicode.com>
2018-01-02 23:16:27 +0200

    Added optional port argument

--- chromecast_youtube.py
+++ chromecast_youtube.py
@@ -1,5 +1,5 @@
 #Copyright Tudor Gheorghiu - Prodicode Solutions S.R.L.
-#Github: https://github.com/Prodicode/chromecast_youtube 
+#Github: https://github.com/Prodicode/chromecast_youtube
 
 import argparse;
 import sys;
@@ -27,6 +27,7 @@ parser = argparse.ArgumentParser(description="A simple python3 script that plays
 requiredArgs = parser.add_argument_group('Required');
 requiredArgs.add_argument('-t', help='The IP of the targeted Chromecast device.', required=True);
 requiredArgs.add_argument('-v', help='The video id of the youtube video which should be played', required=True);
+parser.add_argument('-p', help='The port on which chromecast is running. Default is 8008', default="8008");
 actionArgs = parser.add_argument_group('Actions');
 actionArgs.add_argument("--play", help='Plays the specified Youtube video', action='store_true');
 actionArgs.add_argument("--stop", help='Stops the video', action='store_true');
@@ -38,9 +39,9 @@ if not (args.play or args.stop):
 if (args.play and args.stop):
     sys.exit(err+"Please specify only ONE action: --play or --stop, not both\n");
 
-print(ok+"Checking connection with "+args.t+" on port 8008");
+print(ok+"Checking connection with "+args.t+" on port "+args.p);
 
-response = os.system("ping -p 8008 -c 1 " + args.t);
+response = os.system("ping -p "+args.p+" -c 1 " + args.t);
 
 if response != 0:
     sys.exit(err+"Could not connect to target");
@@ -49,7 +50,7 @@ if response != 0:
 print(ok+"Gathering target information...");
 
 try:
-    r = requests.get('http://'+args.t+":8008/setup/eureka_info?options=detail");
+    r = requests.get('http://'+args.t+":"+args.p+"/setup/eureka_info?options=detail");
     uptime = r.json()['uptime'];
     model = r.json()['detail']['manufacturer'] + " " + r.json()['detail']['model_name'];
     device = r.json()['name'];
@@ -62,13 +63,13 @@ print(info+"Up Time: "+str(uptime));
 
 if args.play:
     try:
-        requests.post('http://'+args.t+':8008/apps/YouTube', data={'v':args.v});
+        requests.post('http://'+args.t+':'+args.p+'/apps/YouTube', data={'v':args.v});
     except:
         sys.exit(err+"Error sending Youtube video");
     print(ok+"Sent video "+args.v+" to "+device);
 else:
     try:
-        requests.delete('http://'+args.t+':8008/apps/YouTube');
+        requests.delete('http://'+args.t+':'+args.p+'/apps/YouTube');
     except:
         sys.exit(err+"Error stopping video");
     print(ok+"Stopped video on "+device);
